34038e2c586cf6824c0eb39926345c066007e0fa
ilker <57532341+ilkerozgen@users.noreply.github.com>
2023-12-03 14:10:46 +0300

    update

--- api/Browsify.py
+++ api/Browsify.py
@@ -2,7 +2,6 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import *
 from PyQt5.QtWebEngineWidgets import *
-from PyQt5.QtGui import QPixmap
 
 from frontend.Styles import BrowsifyStyles
 
